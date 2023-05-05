2240990ec49ae9af319bf8bd31ff2aabc4458a6f
20201TIIMI0306 <nycollydonascimentomendes@gmail.com>
2023-05-05 08:48:17 -0300

    atividade para correção Atividade rodando

--- app/src/main/java/mendes/nascimento/nycolly/appemail/MainActivity.java
+++ app/src/main/java/mendes/nascimento/nycolly/appemail/MainActivity.java
@@ -2,10 +2,16 @@ package mendes.nascimento.nycolly.appemail;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,17 +19,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button Enviar= findViewById(R.id.btnEnviar);
-        Enviar.setOnClickListener(new View.OnClickListener() {
+        //DEFIIÇÃO DO BOTÃO
+        Button btnEnvir= findViewById(R.id.btnEnviar);
+        // DEFINIÇÃO DO CLICK DO BOTÃO
+        btnEnvir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText email= findViewById(R.id.etDigiteEmail);
-                EditText Assunto = findViewById(R.id.etAssunto);
+                //registrando od dados informados pelo usuário
+                EditText etEmail = (EditText) findViewById(R.id.etEmail);
+                String email = etEmail.getText().toString();
+
+                EditText etAssunto =(EditText) findViewById(R.id.etAssunto);
+                String assunto = etAssunto.getText().toString();
+
+                EditText etTexto =(EditText) findViewById(R.id.etTexto);
+                String texto = etTexto.getText().toString();
 
 
+                Intent I = new Intent(Intent.ACTION_SENDTO);
+                I.setData(Uri.EMPTY.parse("mailto:"));
 
+                String[] emails = new String[] {email} ;
+                I.putExtra(Intent.EXTRA_EMAIL,emails);
+                I.putExtra(Intent.EXTRA_SUBJECT, assunto);
+                I.putExtra(Intent.EXTRA_TEXT,texto);
+                try {
+                    startActivity(Intent.createChooser(I, "Escolha o APP"));
+                }
+
+
+                catch (ActivityNotFoundException e ) {
+                    Toast.makeText(MainActivity.this,"Não há nenhum app que posso realizar essa operação", Toast.LENGTH_LONG).show();
+                    }
+
+                
             }
         });
+
     }
 }
